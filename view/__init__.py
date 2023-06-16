f2a82164601c4d80f3e3a43aa10d3290ed8c4945
aleksey.krasnov <krasnov.80@mail.ru>
2023-06-16 11:44:07 +0300

    Версия с семинара

---
+++ view/__init__.py
@@ -0,0 +1,4 @@
+from .console import menu, show_contacts, print_message, input_contacts, input_return
+from .text import *
+
+__all__ = ['menu', 'show_contacts', 'input_contacts', 'input_return', 'print_message']
