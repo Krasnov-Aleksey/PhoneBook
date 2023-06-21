49310f3c94a6454842402d10a103ac17f953e1c1
aleksey.krasnov <krasnov.80@mail.ru>
2023-06-21 13:09:06 +0300

    Добавил класс Сontact

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, uid: int, name: str, phone: str, comment: str):
+        self.uid = uid
+        self.name = name
+        self.phone = phone
+        self.comment = comment
+
